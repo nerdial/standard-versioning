fc25bd14052876b9d63a80c5f274437e0ac0bb44
Vahid <3176766+nerdial@users.noreply.github.com>
2019-04-14 16:01:51 +0430

    Add travis ci and simple phpunit test

---
+++ tests/SimpleTest.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class SimpleTest extends TestCase
+{
+    public function testSimpleTest(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    
+}
+
+
+
+
